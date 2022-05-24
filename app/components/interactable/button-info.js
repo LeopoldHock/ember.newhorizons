2577d6b583123db86c1a282ee682697ac00e1ad3
LeopoldHock <ameisenfutter@outlook.com>
2022-05-24 16:48:19 +0200

    renamed onClick handler

--- app/components/interactable/button-info.js
+++ app/components/interactable/button-info.js
@@ -3,18 +3,16 @@
 // Description:
 // Controller for component interactable::button.
 //----------------------------------------------------------------------------//
-import InteractableComponent from './interactable';
+import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import MessageService from 'new-horizons/services/message-service';
-import classic from 'ember-classic-decorator';
 
-@classic
-export default class DropdownComponent extends InteractableComponent {
+export default class DropdownComponent extends Component {
     @service manager;
     @service stellarpedia;
 
-    @action onClick() {
+    @action handleClick() {
         let articleId = this.manager.database.transformId(this.article);
         let split = articleId.split("/");
         if (!Array.isArray(split) || split.length < 3) {
