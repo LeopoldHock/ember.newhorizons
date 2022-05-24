7b1501b48a1b497272b7217a3a99bef0f523861b
LeopoldHock <ameisenfutter@outlook.com>
2022-05-24 16:51:27 +0200

    added test file for info button

---
+++ tests/integration/components/interactable/button-info-test.js
@@ -0,0 +1,14 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | interactable/button-info', function (hooks) {
+  setupRenderingTest(hooks);
+
+  // test('it renders', async function (assert) {
+  //   this.set("articleId", "basic-rules/introduction/a-short-story-of-the-solar-system");
+  //   await render(hbs`<Interactable::ButtonInfo />`);
+  //   assert.dom(this.element.firstChild.firstChild).hasTagName("button");
+  // });
+});
